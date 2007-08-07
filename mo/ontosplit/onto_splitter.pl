d0904f342536ad7eeb9dc7840edb72c844a082bd
Yves Raimond <yves@dbtune.org>
2007-08-07 22:19:28 +0000

    - added a small prolog program to split an ontology between core and external components

---
+++ mo/ontosplit/onto_splitter.pl
@@ -0,0 +1,64 @@
+:- module(onto_splitter,[split/0]).
+
+/**
+ * Just a util to split the ontology between terms defined by the document
+ * and terms defined outside it
+ *
+ * Yves Raimond, C4DM, QMUL, 2007
+ */
+
+:- use_module(library('semweb/rdf_db')).
+:- use_module(library('rdf')).
+
+
+/**
+ * Adjust these to your needs
+ */
+document('musicontology.rdfs').
+primary_ns('http://purl.org/ontology/mo/').
+output_core('musicontology-core.rdfs').
+output_external('musicontology-external.rdfs').
+
+/**
+ * Top-level goal
+ */
+split :- 
+	load,
+	core,
+	external.
+
+/**
+ * Load the document
+ */
+load :- 
+	document(Doc),
+	rdf_db:rdf_load(Doc).
+
+/**
+ * Extract and export the core
+ */
+core :- 
+	primary_ns(NS),
+	findall(rdf(A,P,O),
+		(rdf_db:rdf(A,rdfs:isDefinedBy,NS),rdf_db:rdf(A,P,O)),
+		TriplesCore),
+	output_core(Core),
+	open(Core,write,CoreStream),
+	rdf_write_xml(CoreStream,TriplesCore),
+	close(CoreStream).
+
+/**
+ * Extract and export the externals
+ */
+external :-
+	primary_ns(NS),
+	findall(rdf(A,P,O),
+		(rdf_db:rdf(A,rdfs:isDefinedBy,NS2),NS2\==NS,rdf_db:rdf(A,P,O)),
+		TriplesExternal),
+	output_external(External),
+	open(External,write,ExternalStream),
+	rdf_write_xml(ExternalStream,TriplesExternal),
+	close(ExternalStream).
+
+		
+
