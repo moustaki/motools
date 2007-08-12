86ab3a6a61eac810c1c33e1d03d742c4ccfa0010
Chris Sutton <chris@chrissutton.org>
2007-08-12 09:21:10 +0000

    First checkin of MO Python interface generator

---
+++ mopy/PropertySet.py
@@ -0,0 +1,37 @@
+"""
+PropertySet.py
+
+Created by Chris Sutton on 2007-08-11.
+Copyright (c) 2007 Chris Sutton. All rights reserved.
+"""
+
+class PropertySet(set):
+
+	def __init__(self, propertyURI, validTypes, allowLits):
+#		self.s = set()
+		set.__init__(self)
+		self.propertyURI = propertyURI
+		self.validTypes = validTypes
+		self.allowLits = allowLits
+		self.Lits = (str, int, list, float) # Any more ?
+	#
+	# Set functions :
+	#	
+	def add(self, o):
+		# type check :
+		print "type checking against : "+str(self.validTypes)
+		if self.allowLits:
+			print "(lits allowed)"
+		if not ((self.allowLits and isinstance(o, self.Lits))\
+				or isinstance(o, self.validTypes)\
+				):
+			raise TypeError("Invalid type ! Expected one of : "+str(self.validTypes))
+		set.add(self,o)
+	
+	def get(self):
+		print "in custom get()"
+		return self
+
+	def set(self, v):
+		self.clear()
+		self.add(v)
---
+++ mopy/genpy.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+genpy.py
+
+Generate model.py, which defines a Python class for each class in the Music Ontology.
+
+Created by Chris Sutton on 2007-08-10.
+Copyright (c) 2007 Chris Sutton. All rights reserved.
+"""
+
+import sys
+import time
+import os
+from os import mkdir
+import rdflib; from rdflib import RDF, RDFS, BNode
+from rdflib.Collection import Collection
+
+class Generator:
+	def __init__(self, graph, target_class):
+		self.graph = graph
+		self.c = target_class
+		[self.ns, self.name] = self.graph.qname(self.c).split(":")
+		self.filename = "model.py"
+		self.out = []
+		self.write = self.out.append
+		self.hdr = ""
+		self.classDef=""
+		self.init ="\tdef __init__(self,URI=None):\n"
+		self.props = ""
+		self.utils = ""
+
+		# Do we have any non-inherited properties ?
+		self.properties = self.getProperties()
+		self.haveProperties = len(self.properties) > 0
+
+	def printAll(self):
+		self.addHeader()
+		self.addClassDef()
+		self.addInit()
+		self.addProperties()
+		self.addUtils()
+		self.write(self.classDef)
+		self.write(self.hdr)
+		self.write(self.init)
+		self.write(self.props)
+		self.write(self.utils)
+		self.out = "".join(self.out)
+
+
+	def addHeader(self):
+		self.hdr = '\t"""\n'
+		self.hdr+="\t"+self.ns+":"+self.name+"\n"
+		for comment in self.graph.objects(self.c, RDFS.comment):
+			self.hdr+="\t"+comment+"\n"
+		self.hdr+= '\t"""\n'
+
+	def addClassDef(self):
+		cd=""
+		parentURIs = self.getParents()
+		parentqnames = [self.graph.qname(p) for p in parentURIs]
+		parentpynames = [ClassQNameToPyClassName(q) for q in parentqnames]
+		parentpynames.sort() # For consistency across runs
+		self.classDef+="\nclass "+self.name+"("
+		if len(parentpynames)==0:
+			cd+="object"
+		else:
+			cd+=", ".join(parentpynames)
+		cd+="):\n"
+		self.classDef += cd
+		
+
+	def addInit(self):
+		parentURIs = self.getParents()
+		parentqnames = [self.graph.qname(p) for p in parentURIs]
+		parentpynames = [ClassQNameToPyClassName(q) for q in parentqnames]
+		if len(parentpynames)>0:
+			self.init+="\t\t# Initialise parents\n"
+		for p in parentpynames:
+			self.init+="\t\t"+p+".__init__(self)\n"
+		
+		if self.haveProperties:
+			self.init+="\t\tself.props = getattr(self,\"props\",{}) # Initialise if a parent class hasn't already\n"
+		self.init+="\t\tself.URI = URI\n"
+			
+	def addProperties(self):
+		if self.haveProperties:
+			self.props+="\n\n\t# Python class properties to wrap the PropertySet objects\n"
+		OWL=rdflib.Namespace((n[1] for n in self.graph.namespaces() if n[0]=="owl").next())
+		for prop in self.properties:
+				propname = PropQNameToPyName(self.graph.qname(prop))
+				URIstr = str(prop)
+				validTypes = ""
+				rTypes = self.graph.objects(prop, RDFS.range)
+				rTypeNames = []
+				allowLits=False
+				for rT in rTypes:
+					if isinstance(rT, BNode):
+						try:
+							un = (self.graph.objects(rT,OWL["unionOf"])).next()
+							col = Collection(self.graph, un)
+							for c in col:
+								rTypeNames.append(ClassQNameToPyClassName(self.graph.qname(c)))
+						except StopIteration:
+							print "Unhandled Blind Node in addProperties ! No unionOf found. Triples :"
+							print "\n".join(list(self.graph.triples(rT,None,None)))
+							raise Exception("Unhandled Blind Node in addProperties !")
+					else:
+						rTypeNames.append(ClassQNameToPyClassName(self.graph.qname(rT)))
+				if "Literal" in rTypeNames:
+					allowLits=True
+					rTypeNames.remove("Literal")
+				if len(rTypeNames) > 1:
+					validTypes="("+",".join(rTypeNames)+")"
+				elif len(rTypeNames) == 1:
+					validTypes=rTypeNames[0]
+				else:
+					validTypes="None"
+
+				self.init+="\t\tself.props[\""+propname+"\"] = PropertySet(\""+URIstr+"\", "+validTypes
+				self.init+=", "+str(allowLits)+")\n"
+				# Wrap the PropertySet up to be usable and protected :
+				self.props+="\t" + propname + " = property(fget=lambda x: x.props[\"" + propname + "\"].get()"\
+														", fset=lambda x,y : x.props[\"" + propname + "\"].set(y)"\
+														", fdel=None, doc=propDocs[\""+propname+"\"])\n"
+	def addUtils(self):
+		self.utils+="\n\t# Utility methods\n" # TODO : serialisation routine here ?
+		self.utils+="\tpass\n\n"
+	
+	def getProperties(self):
+		props=[]
+		OWL=rdflib.Namespace((n[1] for n in self.graph.namespaces() if n[0]=="owl").next())
+		for prop in self.graph.subjects(RDF.type, RDF.Property):
+			# Simple case : Named explicitly in property's domain
+			if len(list(self.graph.triples((prop,RDFS.domain,self.c)))) > 0:
+				props.append(prop)
+			# Harder case : Named in a collection in property's domain
+			for bn in (o for (s,p,o) in self.graph.triples((prop,RDFS.domain,None)) if isinstance(o, BNode)):
+				try:
+					un = (self.graph.objects(bn,OWL["unionOf"])).next()
+					domain = Collection(self.graph, un)
+					if self.c in domain:
+						props.append(prop)
+				except StopIteration:
+					print "Unhandled Blind Node in getProperties ! No unionOf found. Triples :"
+					print "\n".join(list(self.graph.triples(bn,None,None)))
+					raise Exception("Unhandled Blind Node in getProperties")
+		props.sort() # Aid consistency of generated code
+		return props
+
+	def getParents(self):
+		p = list(self.graph.objects(self.c, RDFS.subClassOf))
+		if (self.c != RDFS.Resource) and (len(p) == 0):
+			p.append(RDFS.Resource)
+		p.sort()
+		return p
+
+def PropQNameToPyName(qname):
+#	return qname.replace(":","_") # Probably don't need namespace in property names
+	return qname.split(":")[1]
+	
+def ClassQNameToPyModuleName(qname):
+	return qname.replace(":",".")
+def ClassQNameToPyClassName(qname):
+	return qname.split(":")[1]
+
+
+def main():
+	spec_g = rdflib.ConjunctiveGraph()
+	print "Loading ontology documents..."
+	spec_g.load("../mo/rdf/musicontology.rdfs")
+	spec_g.load("extras.rdfs")
+
+	model = open("model.py", "w")
+	model.write("""
+# ===================================================================
+# = model.py - Core and External Classes of the Music Ontology 
+# =            Generated automatically on """+time.asctime()+"""
+# ===================================================================\n\n\n""")
+
+	model.write("from PropertySet import PropertySet\n\n")
+	classes = list(spec_g.subjects(RDF.type, RDFS.Class))
+	classtxt = {}
+	parents = {}
+	
+	#
+	# Generate text
+	#
+	model.write("\n# ======================== Property Docstrings ====================== \n\n")
+	model.write("propDocs = {}\n")
+	props = list(spec_g.subjects(RDF.type, RDF.Property))
+	for p in props:
+		doc = "".join(spec_g.objects(p, RDFS.comment))
+		if len(doc) > 0:
+			model.write("propDocs[\""+PropQNameToPyName(spec_g.qname(p))+"\"]=\\\n\"\"\""+doc.strip()+"\"\"\"\n")
+		else:
+			model.write("propDocs[\""+PropQNameToPyName(spec_g.qname(p))+"\"]=\"\"\n")
+		
+	model.write("\n\n\n\n# ========================  Class Definitions  ====================== \n")
+	for c in classes:
+		print "processing " + str(c)
+		g = Generator(spec_g, c)
+		g.printAll()
+		classtxt[str(c)] = g.out
+		parents[str(c)] = [str(p) for p in g.getParents()]
+
+	#
+	# Serialise classes in an appropriate order
+	#
+	classes = [str(c) for c in classes]
+	classes.sort() # Ensure serialisation order is reasonably consistent across runs
+	n=1; lastlen = len(classes)+1
+	while (len(classes) > 0) and len(classes) < lastlen:
+		lastlen = len(classes)
+		print("pass "+ str(n))
+		for c in classes:
+			if len(parents[c]) == 0: # Write out orphans immediately
+				model.write(classtxt[c])
+				print(" wrote "+c)
+				classes.remove(c)
+				for k in classes:    # And abandon any classes who were waiting for us
+					if c in parents[k]:
+						parents[k].remove(c)
+		n+=1
+	
+	if len(classes) > 0:
+		print "Couldn't find a serialisation order ! Remaining classes : " + "\n".join(classes)
+			
+	model.close()
+
+if __name__ == '__main__':
+	main()
+
