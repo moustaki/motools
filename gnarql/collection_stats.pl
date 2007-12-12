301c16728b9c4c318ec0d65683ea44ac7fe19bb2
Chris Sutton <chris@chrissutton.org>
2007-12-12 16:18:01 +0000

    Added artist,album,track stats to sidebar

--- gnarql/collection_stats.pl
+++ gnarql/collection_stats.pl
@@ -2,12 +2,22 @@
 
 :- use_module(library('semweb/rdf_db')).
 
+% Naive approaches :
+% (probly want eg. to check they're associated with files on disk rather than just discovered on the web)
 
-num_artists(NA) :- findall((S,_,_,_),
-						(rdf_db:rdf(S,rdf:type,mo:'MusicArtist',_), \+rdf_is_bnode(S)),
+:- rdf_meta num_by_type(-,r).
+num_by_type(N, Type) :- findall((S,_,_,_),
+						(rdf_db:rdf(S,rdf:type,Type,_), \+rdf_is_bnode(S)),
 						Quads
 						),
 						list_to_set(Quads, QuadSet),
-				length(QuadSet, NA).
+						length(QuadSet, N).
 
-stats(X) :- num_artists(NA), atom_concat(NA,' known artists', X).
+
+num_artists(NA) :- num_by_type(NA,mo:'MusicArtist').
+num_albums(NB)  :- num_by_type(NB,mo:'Record').
+num_tracks(NT)  :- num_by_type(NT,mo:'Track').
+
+
+stats(X) :- num_artists(NA), num_albums(NB), num_tracks(NT),
+			sformat(X, '~w artists, ~w albums, ~w tracks', [NA,NB,NT]).
