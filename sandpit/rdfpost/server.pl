9f86e2b2f8e3f854a5da9d6993df1ba2d740f13e
Yves Raimond <yves@dbtune.org>
2008-02-01 13:13:41 +0000

    Small script to allow posting of rdf files

---
+++ sandpit/rdfpost/server.pl
@@ -0,0 +1,33 @@
+:- module(server, [server/0]).
+
+:- use_module(library('http/thread_httpd')).
+:- use_module(library('http/http_client')).
+:- use_module(library('http/http_mime_plugin')).
+
+
+ns('http://dbtune.org/people/').
+
+
+server(Port, Options) :-
+        http_server(reply,[ port(Port),timeout(20)| Options]).
+
+
+reply(Request) :-
+	member(method(post), Request), !,
+	http_read_data(Request,Data,[]),
+	member((turtle=Turtle),Data),
+	member((name=Name),Data),
+	((exists_directory(Name),!);(make_directory(Name))),
+	ns(NS),
+	format(atom(File),'~w/content.ttl',[Name]),
+	open(File,write,S),
+	write(S,Turtle),
+	close(S),
+	atom_concat(NS,File,URL),
+	throw(http_reply(see_other(URL),[])).
+
+
+
+
+server :- server(1112,[]).
+
