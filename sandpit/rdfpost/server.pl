0689984b15479c502be6130dabf01ca1e1c6819d
Yves Raimond <yves@dbtune.org>
2008-02-01 13:50:00 +0000

    Parsing

--- sandpit/rdfpost/server.pl
+++ sandpit/rdfpost/server.pl
@@ -3,7 +3,8 @@
 :- use_module(library('http/thread_httpd')).
 :- use_module(library('http/http_client')).
 :- use_module(library('http/http_mime_plugin')).
-
+:- use_module(library('semweb/rdf_db')).
+:- use_module(library('semweb/rdf_turtle')).
 
 ns('http://dbtune.org/people/').
 
@@ -17,18 +18,27 @@ reply(Request) :-
 	http_read_data(Request,Data,[]),
 	format(user_output,'~w\n',[Data]),
 	member((turtle=Turtle),Data),
+	parse_turtle(Turtle,Triples),
 	member((name=Name),Data),
 	((exists_directory(Name),!);(make_directory(Name))),
 	ns(NS),
-	format(atom(File),'~w/content.ttl',[Name]),
+	format(atom(File),'~w/content.rdf',[Name]),
 	format(user_output,' - Dumping ~w\n',[File]),
 	open(File,write,S),
-	write(S,Turtle),
+	parse_turtle(Turtle,Triples),
+	rdf_write_xml(S,Triples),
 	close(S),
 	atom_concat(NS,File,URL),
 	throw(http_reply(see_other(URL),[])).
 
 
+parse_turtle(Turtle,Triples) :-
+	tmp_file(ttl,Tmp),
+	open(Tmp,write,S),
+	write(S,Turtle),
+	close(S),
+	rdf_load_turtle(Tmp,Triples,[base_uri('')]).
+	
 
 
 server :- server(1112,[]).
