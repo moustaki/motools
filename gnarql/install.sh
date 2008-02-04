2912c4311208d9bc71c2d7d66a3ee17918f0ec33
Yves Raimond <yves@dbtune.org>
2008-02-04 11:13:12 +0000

    Fixed the conflict with current ClioPatria

--- gnarql/install.sh
+++ gnarql/install.sh
@@ -6,5 +6,9 @@
 
 git clone git://gollem.science.uva.nl/home/git/ClioPatria.git
 
-cp SeRQL/* ClioPatria/SeRQL/
+cp SeRQL/*.html ClioPatria/SeRQL/
+cp SeRQL/*.css ClioPatria/SeRQL/
+cp SeRQL/rdf_html.pl ClioPatria/SeRQL/
+cp SeRQL/http_user.pl ClioPatria/SeRQL/
+
 
