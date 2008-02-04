ca6e1cd6b61b70d1d9132d7aee95d2af0f657f6d
Yves Raimond <yves@dbtune.org>
2008-02-04 10:28:40 +0000

    Install script

---
+++ gnarql/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install dependencies for GNARQL
+# Prerequesites:
+#   * Install GIT
+
+git clone git://gollem.science.uva.nl/home/git/ClioPatria.git
+
+cp SeRQL/* ClioPatria/SeRQL/
+
