13fbc1336197e602414c23f79907001bb37954d0
Yves Raimond <yves@dbtune.org>
2008-06-04 15:58:30 +0000

    Lastfm event wrapper

---
+++ lastfm/lastfm_events.pl
@@ -0,0 +1,84 @@
+:- module(lastfm_events,[events_rdf/2]).
+
+:- use_module(library('http/http_open')).
+:- use_module(library('semweb/rdf_db')).
+
+:- use_module(namespaces).
+:- use_module(config).
+
+events_rdf(User,Triples) :-
+	events_xml(User,Xml),
+	member(element(rss,_,Rss),Xml),!,
+	member(element(channel,_,Channel),Rss),
+	host(Host),
+	format(atom(UserUri),'~w/~w',[Host,User]),
+	findall(Rdf,(member(element(item,_,EventItem),Channel),event_rdf(EventItem,UserUri,Rdf)),T),
+	flatten(T,Triples).
+	
+
+event_rdf(Event,UserUri,[
+		rdf(EventUri,dc:title,literal(Title))
+	,	rdf(EventUri,rdf:type,mo:'Performance')
+	,	rdf(UserUri,lfm:recommendation,EventUri)
+	,	rdf(EventUri,foaf:homepage,literal(Page))
+	,	rdf(EventUri,dc:description,literal(Desc))
+	,	rdf(EventUri,rdf:type,event:'Event')
+	,	rdf(EventUri,event:time,TimeUri)
+	,	rdf(EventUri,event:place,PlaceUri)
+	,	rdf(PlaceUri,rdf:type,wgs:'Point')
+	,	rdf(PlaceUri,dc:title,literal(Address))
+	,	rdf(PlaceUri,foaf:homepage,LocationURI)
+	,	rdf(PlaceUri,wgs:lat,literal(Lat))
+	,	rdf(PlaceUri,wgs:long,literal(Long))
+	,	rdf(TimeUri,rdf:type,tl:'Interval')
+	,	rdf(TimeUri,tl:start,literal(type('http://www.w3.org/2001/XMLSchema#dateTime',Start)))
+	,	rdf(TimeUri,tl:end,literal(type('http://www.w3.org/2001/XMLSchema#dateTime',End)))
+	,	rdf(EventUri,lfm:publication_date,literal(type('http://www.w3.org/2001/XMLSchema#dateTime',PubDate)))
+	]) :-
+	member(element(title,_,[Title]),Event),
+	member(element(link,_,[Page]),Event),
+	member(element(description,_,[Desc]),Event),
+	member(element(pubDate,_,[PubDate]),Event),
+	member(element('xcal:dtstart',_,[Start]),Event),
+	member(element('xcal:dtend',_,[End]),Event),
+	member(element('xcal:location',_,[LocationURI]),Event),
+	rdf_bnode(EventUri),rdf_bnode(TimeUri),rdf_bnode(PlaceUri),
+	parse_desc(Desc,Place),
+	Place\='',
+	format(user_error,'Geocoding ~w\n',Place),
+	google_geo(Place,Address,Lat,Long),
+	format(user_error,'Found ~w,~w\n',[Lat,Long]).
+	%host(Host),
+	%atom_concat('http://www.last.fm/event/',EventId,Page),
+	%concat_atom([Host,'/','event','/',EventId],Id).
+	
+
+
+events_xml(User,Xml) :-
+	format(atom(URL),'http://ws.audioscrobbler.com/1.0/user/~w/eventsysrecs.rss',[User]),
+	http_open(URL,Stream,[]),
+	load_xml_file(Stream,Xml),
+	close(Stream).
+
+parse_desc(Desc,Location) :-
+	concat_atom([_,T|_],'Location: ',Desc),
+	concat_atom([Location|_],'\n',T),!.
+parse_desc(_,'').
+
+api_key('ABQIAAAAu0AMQcAkvqfViJpEeSH_-hT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQ0_Z6CDgX2Q08wvAh1aYjckybfeA').
+google_geo(Literal,Address,Lat,Long) :-
+	api_key(Key),www_form_encode(Literal,LE),
+	format(atom(URL),'http://maps.google.com/maps/geo?q=~w&output=kml&key=~w',[LE,Key]),
+	http_open(URL,Stream,[]),
+	load_xml_file(Stream,Xml),
+	member(element(kml,_,Kml),Xml),
+	close(Stream),
+	member(element('Response',_,Response),Kml),
+	member(element('Placemark',_,Placemark),Response),
+	member(element('address',_,[Address]),Placemark),
+	member(element('Point',_,Point),Placemark),
+	member(element('coordinates',_,[Coord]),Point),
+	concat_atom([Lat,Long,_Alt],',',Coord).
+	
+
+
--- lastfm/namespaces.pl
+++ lastfm/namespaces.pl
@@ -7,4 +7,8 @@
 :- rdf_register_ns(lfm,'http://purl.org/ontology/last-fm/').
 :- rdf_register_ns(dc,'http://purl.org/dc/elements/1.1/').
 :- rdf_register_ns(foaf,'http://xmlns.com/foaf/0.1/').
+:- rdf_register_ns(event,'http://purl.org/NET/c4dm/event.owl#').
+:- rdf_register_ns(tl,'http://purl.org/NET/c4dm/timeline.owl#').
+:- rdf_register_ns(wgs,'http://www.w3.org/2003/01/geo/wgs84_pos#').
+:- rdf_register_ns(dc,'http://purl.org/dc/elements/1.1/').
 
