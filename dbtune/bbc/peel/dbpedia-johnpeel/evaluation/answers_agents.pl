643a88c86e83b68ef825b50054c8c1c5a1bc115b
Yves Raimond <yves@dbtune.org>
2008-06-07 00:24:09 +0000

    Result of the evaluation script for agents using metho 2.

---
+++ dbtune/bbc/peel/dbpedia-johnpeel/evaluation/answers_agents.pl
@@ -0,0 +1,61 @@
+pnl_fail('http://dbtune.org/bbc/peel/artist/1330','It is available at http://dbpedia.org/page/Neon_(band), but label is different - neon, band - the type is also none of the expected ones').
+pnl_ok('http://dbtune.org/bbc/peel/artist/896').
+pnl_fail('http://dbtune.org/bbc/peel/artist/171','Non-matching label - called the bays in dbpedia').
+pnl_ok('http://dbtune.org/bbc/peel/artist/2121').
+pl_ok('http://dbtune.org/bbc/peel/artist/2107','http://dbpedia.org/resource/Tricky').
+pnl_ok('http://dbtune.org/bbc/peel/artist/2102').
+pnl_ok('http://dbtune.org/bbc/peel/artist/869').
+pl_ok('http://dbtune.org/bbc/peel/artist/843','http://dbpedia.org/resource/Helen_Love_(band)').
+pl_ok('http://dbtune.org/bbc/peel/artist/2074','http://dbpedia.org/resource/Tim_Hart').
+pl_ok('http://dbtune.org/bbc/peel/artist/2055','http://dbpedia.org/resource/Therapy?').
+pnl_ok('http://dbtune.org/bbc/peel/artist/820').
+pnl_fail('http://dbtune.org/bbc/peel/artist/816','It is in dbpedia with the right label in european languages, but THE gun club in english').
+pnl_ok('http://dbtune.org/bbc/peel/artist/390').
+pnl_fail('http://dbtune.org/bbc/peel/artist/386','The label has a . in the end').
+pnl_ok('http://dbtune.org/bbc/peel/artist/370').
+pl_ok('http://dbtune.org/bbc/peel/artist/367','http://dbpedia.org/resource/Chelsea_(band)').
+pl_ok('http://dbtune.org/bbc/peel/artist/362','http://dbpedia.org/resource/Charlie_(band)').
+pl_ok('http://dbtune.org/bbc/peel/artist/1171','http://dbpedia.org/resource/Manic_Street_Preachers').
+pnl_ok('http://dbtune.org/bbc/peel/artist/1975').
+pl_ok('http://dbtune.org/bbc/peel/artist/1970','http://dbpedia.org/resource/The_Motors').
+pnl_ok('http://dbtune.org/bbc/peel/artist/1533').
+pl_ok('http://dbtune.org/bbc/peel/artist/1529','http://dbpedia.org/resource/Radiohead').
+pnl_fail('http://dbtune.org/bbc/peel/artist/1524','Exists in wikipedia with a label radar bros.').
+pnl_fail('http://dbtune.org/bbc/peel/artist/300','Confusing labels, Buckfunk in bbc john peel looks like an alias for si begg, which is actually existing in dbpedia. it has in dbpedia a matching alias Buckfunk 3000, but under another property (alias), which concatenates also Cabbageboy').
+pnl_fail('http://dbtune.org/bbc/peel/artist/275','Exists in dbpedia, but with THE Boys of the Lough as a label. Matching label is in the german spelling.').
+pnl_ok('http://dbtune.org/bbc/peel/artist/110').
+pnl_fail('http://dbtune.org/bbc/peel/artist/1096','Matching label. Matching types. I really have no clue - perhaps added recently to dbpedia?').
+pnl_ok('http://dbtune.org/bbc/peel/artist/669').
+pl_ok('http://dbtune.org/bbc/peel/artist/665','http://dbpedia.org/resource/Fairfield_Parlour').
+pnl_ok('http://dbtune.org/bbc/peel/artist/660').
+pnl_ok('http://dbtune.org/bbc/peel/artist/645').
+pnl_fail('http://dbtune.org/bbc/peel/artist/640','Dbpedia label is spelled Eric B. & Rakim, BBC label is missing the .').
+pnl_ok('http://dbtune.org/bbc/peel/artist/625').
+pl_ok('http://dbtune.org/bbc/peel/artist/144','http://dbpedia.org/resource/Bad_Religion').
+pl_ok('http://dbtune.org/bbc/peel/artist/1435','http://dbpedia.org/resource/Piano_Red').
+pnl_ok('http://dbtune.org/bbc/peel/artist/100').
+pnl_fail('http://dbtune.org/bbc/peel/artist/1420','Doesn''t have any SKOS type in DBpedia, neither infobox. No way to know what it is').
+
+pnl_fail('http://dbtune.org/bbc/peel/artist/2205','Capitalisation of label is different in wikipedia').
+pnl_fail('http://dbtune.org/bbc/peel/artist/2190','Wagon Christ is an alias for Luke Vibert - the rdfs:label doesnt match, alias is amongst other ones').
+pnl_fail('http://dbtune.org/bbc/peel/artist/2167','Label in dbpedia is THE Velvet Underground, only matching label is in the italian version').
+pnl_fail('http://dbtune.org/bbc/peel/artist/932','Matching label in dbpedia is J Church (band)').
+pnl_fail('http://dbtune.org/bbc/peel/artist/928','Matching label in dbpedia is Isan (band)').
+pnl_ok('http://dbtune.org/bbc/peel/artist/913').
+pnl_ok('http://dbtune.org/bbc/peel/artist/2144').
+pnl_fail('http://dbtune.org/bbc/peel/artist/214','Labelled as Black Dog Productions in dbpedia').
+pl_ok('http://dbtune.org/bbc/peel/artist/211','http://dbpedia.org/resource/Bivouac_(band)').
+pnl_fail('http://dbtune.org/bbc/peel/artist/1921','Label is Four Brothers (band) - lacks the THE').
+pnl_ok('http://dbtune.org/bbc/peel/artist/677').
+pl_ok('http://dbtune.org/bbc/peel/artist/672','http://dbpedia.org/resource/Faust_(band)').
+pnl_ok('http://dbtune.org/bbc/peel/artist/663').
+pl_ok('http://dbtune.org/bbc/peel/artist/659','http://dbpedia.org/resource/Extreme_Noise_Terror').
+pnl_ok('http://dbtune.org/bbc/peel/artist/1462').
+pl_fail('http://dbtune.org/bbc/peel/artist/1458','http://dbpedia.org/resource/Play_Dead','Links to a disambiguation page, although it holds a link to the correct band - wrong mbz mapping here as well. Weirdly, the types shouldnt match').
+pnl_fail('http://dbtune.org/bbc/peel/artist/1860','Matching labels, matching types. No clue. Perhaps dbpedia was updated since i ve done the mapping').
+pnl_ok('http://dbtune.org/bbc/peel/artist/1845').
+pnl_fail('http://dbtune.org/bbc/peel/artist/1840','the label in dbpedia is T. Raumschmiere (there is a space)').
+pnl_ok('http://dbtune.org/bbc/peel/artist/1826').
+pnl_ok('http://dbtune.org/bbc/peel/artist/1821').
+pnl_ok('http://dbtune.org/bbc/peel/artist/587').
+pl_ok('http://dbtune.org/bbc/peel/artist/1400','http://dbpedia.org/resource/Part_Chimp').
