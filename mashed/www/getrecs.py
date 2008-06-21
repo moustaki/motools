6157c84f9e0e5e29d286cc2fe8c331b15fc5e008
Kurt Jacobson <kurtjx@gmail.com>
2008-06-21 15:08:52 +0000

    her we go www

--- mashed/www/getrecs.py
+++ mashed/www/getrecs.py
@@ -1,16 +1,20 @@
 #!/home/c4dm/local/bin/python
 
+lastfmURI = "http://dbtune.org/lastfm/"
+
+
 import cgi
 import urllib
+import rdflib
 
 def Main():
 
 	uriQuery = cgi.FieldStorage()                # Get the query string from the URI
 	lastfm = uriQuery.getfirst('lastfm','')
-	#gnat = uriQuery.getfirst('gnat','')
+	gnat = uriQuery.getfirst('gnat','')
 	print "Content-type: text/html\n\n"
 	
 	print lastfm
-	#print gnat
+	print gnat
 
 Main()
