22d4b70c5bde816da5bdf416aedebbc9521f4a8c
Kurt Jacobson <kurtjx@gmail.com>
2008-06-21 16:24:22 +0000

    not working www

--- mashed/www/getrecs.py
+++ mashed/www/getrecs.py
@@ -10,11 +10,19 @@ import rdflib
 def Main():
 
 	uriQuery = cgi.FieldStorage()                # Get the query string from the URI
-	lastfm = uriQuery.getfirst('lastfm','')
-	gnat = uriQuery.getfirst('gnat','')
+	lastfm = uriQuery['lastfm'].value
+	lasttype = uriQuery['lastfm'].type
+	gnat = uriQuery['gnat'].value #.getfirst('gnat','')
+	gnat2 = uriQuery['gnat'].type
+	#lines = gnat.open()#.readlines()
 	print "Content-type: text/html\n\n"
 	
-	print lastfm
-	print gnat
+	print lastfm+ str(lasttype)+"<br>"
+
+	print "getfirst " + str(gnat) + "<br>"
+	print ".type: " + str(gnat2) +"<br>"
+	print dir(gnat)
+	#for line in lines:
+		#print line
 
 Main()
