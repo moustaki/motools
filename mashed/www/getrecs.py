7bc7bb0fceb0b47636c5d71e7ad2c85651c1cac9
Kurt Jacobson <kurtjx@gmail.com>
2008-06-21 18:31:54 +0000

    betaa html

--- mashed/www/getrecs.py
+++ mashed/www/getrecs.py
@@ -1,6 +1,6 @@
 #!/home/c4dm/local/bin/python
 
-lastfmURI = "http://dbtune.org/lastfm/"
+lastfmURI = "http://dbtune.org/last-fm/"
 
 
 import cgi
@@ -11,23 +11,28 @@ import mopy
 def Main():
 
 	uriQuery = cgi.FieldStorage()                # Get the query string from the URI
-	lastfmUser = uriQuery['lastfm'].value
-	
-	gnatFile = uriQuery['gnat'].file.read()
-	print "Content-type: text/html\n\n"
-	f = open('temp.rdf', 'w')
+	if uriQuery.has_key('lastfm') or uriQuery.has_key('gnat'):
+		if uriQuery.has_key('lastfm'):
+			lastfmUser = uriQuery['lastfm'].value
+			# mopy can't handle the last-fm service
+			# lastfmRDF = mopy.importRDFFile(lastfmURI+lastfmUser)
 
 
-	print "tracks: "
-	print "<br><br>"
 
-	for char in gnatFile:
-		f.write(char)
-		#print char
+		if uriQuery.has_key('gnat'):
+			gnatFile = uriQuery['gnat'].file.read()
+			print "Content-type: text/html\n\n"
+			f = open('temp.rdf', 'w')
+			print "gnat tracks: "
+			print "<br><br>"
+			for char in gnatFile:
+				f.write(char)
 
-	f.close()
-	rdf = mopy.importRDFFile('temp.rdf')
-	tracks = rdf.TrackIdx.keys()
-	print tracks
+			f.close()
+			rdf = mopy.importRDFFile('temp.rdf')
+			tracks = rdf.TrackIdx.keys()
+			print tracks
+	else:
+		print "must submit an RDF file"
 
 Main()
