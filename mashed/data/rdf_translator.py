770c4a421f1ddb72928b947c55d5c54e73dad452
Yves Raimond <yves@dbtune.org>
2008-06-21 14:04:55 +0000

    RDF translator

---
+++ mashed/data/rdf_translator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import csv
+import sys
+
+list = []
+
+f = open("brands_artists/brands_artists.txt","rb")
+parser = csv.reader(f,delimiter=' ')
+
+rdf = open("brands_artist.n3","w")
+
+for line in parser:
+	brand = line[0]
+	for k in range(1,len(line)/2):
+		artist = line[2*k-1]
+		playcount = (line[2*k].split(','))[0]
+		rdf_line = "<http://http://bbc-programmes.dyndns.org/programmes"+brand+"> "+"<http://purl.org/ontology/playcount/playcount> [<http://purl.org/ontology/playcount/count> \""+playcount+"\"; <http://purl.org/ontology/playcount/object> <http://dbtune.org/musicbrainz/artist/"+artist+">].\n"
+		rdf.write(rdf_line)
+		
+f.close()
+
