56e13fc5265c23d047a3c1467fd1c546dc5a20d3
Yves Raimond <yves@dbtune.org>
2008-06-22 08:55:06 +0000

    Link to Nich's content

--- mashed/recommender/crawler.py
+++ mashed/recommender/crawler.py
@@ -13,6 +13,8 @@ rt = store.open('db',create=False)
 g = Graph(store)
 #g.load("brands_artist.n3",format='n3')
 
+broadcasts = []
+
 #mbz = SPARQLWrapper("http://dbtune.org/musicbrainz/sparql")
 
 PC = Namespace('http://purl.org/ontology/playcount/')
@@ -66,18 +68,22 @@ WHERE {
 	for brand in brands:
 		print brand
 		g.load(brand)
+		g.commit()
 		q = "SELECT ?e WHERE {<%s> <http://purl.org/ontology/po/episode> ?e.}" % brand
 		print q
 		for e in g.query(q):
 			print e
 			g.load(e[0])
+			g.commit()
 			q2 = "SELECT ?v WHERE {<%s> <http://purl.org/ontology/po/version> ?v.}" % e
 			for v in g.query(q2):
 				g.load(v[0])
-				q3 = "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?start ?dur WHERE {?broadcast <http://purl.org/ontology/po/broadcast_of> <%s>; <http://purl.org/NET/c4dm/event.owl#time> ?time. ?time <http://purl.org/NET/c4dm/timeline.owl#start> ?start; <http://purl.org/NET/c4dm/timeline.owl#duration> ?dur.}" % v
+				g.commit()
+				q3 = "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?start ?dur ?service WHERE {?broadcast <http://purl.org/ontology/po/broadcast_of> <%s>; <http://purl.org/NET/c4dm/event.owl#time> ?time. ?time <http://purl.org/NET/c4dm/timeline.owl#start> ?start; <http://purl.org/NET/c4dm/timeline.owl#duration> ?dur; <http://purl.org/ontology/po/broadcasted_on> ?service.}" % v
 				for row in g.query(q3):
 					if row[0].startswith('2008'):
-						print "start %s duration %s" % row
+						print "start %s duration %s service %s" % row
+						broadcasts.append(create_content_url(row[0],row[1],parse_service(row[2])))
 		
 
 
@@ -87,7 +93,11 @@ WHERE {
 #g.load(seed)
 
 
+def parse_service(uri):
+	return (uri.split("http://bbc-programmes.dyndns.org/"))[1]
 
+def create_content_url(start, duration, service):
+	return "http://10.88.88.88/archives/"+service+"_"+start+"_"+duration+".mp2"
 
 
 seed = argv[1]
