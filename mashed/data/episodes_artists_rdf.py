d097b4e009ccda6b20d2fc8cd5f3e5979e1026e0
Yves Raimond <yves@dbtune.org>
2008-06-22 11:55:09 +0000

    Episodes/artists rdf

---
+++ mashed/data/episodes_artists_rdf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import csv
+import sys
+
+list = []
+
+f = open("episodes_artists/episodes_artists.txt","rb")
+parser = csv.reader(f,delimiter=' ')
+
+rdf = open("episodes_artist.n3","w")
+
+for line in parser:
+	brand = line[0]
+	for k in range(1,len(line)/2):
+		artist = line[2*k-1]
+		playcount = (line[2*k].split(','))[0]
+		rdf_line = "<http://bbc-programmes.dyndns.org/programmes/"+brand+"#brand> "+"<http://purl.org/ontology/playcount/playcount> [<http://purl.org/ontology/playcount/count> \""+playcount+"\"; <http://purl.org/ontology/playcount/object> <http://dbtune.org/musicbrainz/resource/artist/"+artist+">].\n"
+		rdf.write(rdf_line)
+		
+f.close()
+
