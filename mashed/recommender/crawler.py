02ef9a633422f3aa1f77a20fa4c5b3a3c384f982
Kurt Jacobson <kurtjx@gmail.com>
2008-06-22 11:30:09 +0000

    craw print urls

--- mashed/recommender/crawler.py
+++ mashed/recommender/crawler.py
@@ -95,7 +95,7 @@ WHERE {
 						else: 
 							s = row[2]
 						url = create_content_url(row[0],row[1],parse_service(s))
-						print "<a href='"+url+">"+url+"</a>"
+						print "<a href='"+url+'">"+url+"</a>"
 						print "<br/>"
 						broadcasts.append(url)
 						if len(broadcasts)>5:
