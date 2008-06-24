077573c90f96d8e42fb2b636b4d5844044406f9e
Yves Raimond <yves@dbtune.org>
2008-06-24 08:02:50 +0000

    URIs for playcounts

--- mashed/data/brands_artists_rdf.py
+++ mashed/data/brands_artists_rdf.py
@@ -8,13 +8,14 @@ f = open("brands_artists/brands_artists.txt","rb")
 parser = csv.reader(f,delimiter=' ')
 
 rdf = open("brands_artist.n3","w")
+base = "http://dbtune.org/bbc/playcount/"
 
 for line in parser:
 	brand = line[0]
 	for k in range(1,len(line)/2):
 		artist = line[2*k-1]
 		playcount = (line[2*k].split(','))[0]
-		rdf_line = "<http://bbc-programmes.dyndns.org/programmes/"+brand+"#brand> "+"<http://purl.org/ontology/playcount/playcount> [<http://purl.org/ontology/playcount/count> \""+playcount+"\"; <http://purl.org/ontology/playcount/object> <http://dbtune.org/musicbrainz/resource/artist/"+artist+">].\n"
+		rdf_line = "<http://bbc-programmes.dyndns.org/programmes/"+brand+"#brand> "+"<http://purl.org/ontology/playcount/playcount> <"+base+brand+">. <"+base+brand+"> <http://purl.org/ontology/playcount/count> \""+playcount+"\"; <http://purl.org/ontology/playcount/object> <http://dbtune.org/musicbrainz/resource/artist/"+artist+">.\n"
 		rdf.write(rdf_line)
 		
 f.close()
--- mashed/data/episodes_artists_rdf.py
+++ mashed/data/episodes_artists_rdf.py
@@ -6,6 +6,7 @@ list = []
 
 f = open("episodes_artists/episodes_artists.txt","rb")
 parser = csv.reader(f,delimiter=' ')
+base = "http://dbtune.org/bbc/playcount/"
 
 rdf = open("episodes_artist.n3","w")
 
@@ -14,7 +15,7 @@ for line in parser:
 	for k in range(1,len(line)/2):
 		artist = line[2*k-1]
 		playcount = (line[2*k].split(','))[0]
-		rdf_line = "<http://bbc-programmes.dyndns.org/programmes/"+brand+"#brand> "+"<http://purl.org/ontology/playcount/playcount> [<http://purl.org/ontology/playcount/count> \""+playcount+"\"; <http://purl.org/ontology/playcount/object> <http://dbtune.org/musicbrainz/resource/artist/"+artist+">].\n"
+		rdf_line = "<http://bbc-programmes.dyndns.org/programmes/"+brand+"#episode> "+"<http://purl.org/ontology/playcount/playcount> <"+base+brand+">. <"+base+brand+"> <http://purl.org/ontology/playcount/count> \""+playcount+"\"; <http://purl.org/ontology/playcount/object> <http://dbtune.org/musicbrainz/resource/artist/"+artist+">.\n"
 		rdf.write(rdf_line)
 		
 f.close()
