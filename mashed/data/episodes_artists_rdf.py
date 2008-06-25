ffd74bcff6478dfa845036413519fcce80b84349
Yves Raimond <yves@dbtune.org>
2008-06-25 11:00:32 +0000

    Typing

--- mashed/data/episodes_artists_rdf.py
+++ mashed/data/episodes_artists_rdf.py
@@ -9,13 +9,14 @@ parser = csv.reader(f,delimiter=' ')
 base = "http://dbtune.org/bbc/playcount/"
 
 rdf = open("episodes_artist.n3","w")
+rdf.write("@prefix xsd: <http://www.w3.org/2001/XMLSchema#>.\n\n")
 
 for line in parser:
 	brand = line[0]
 	for k in range(1,len(line)/2):
 		artist = line[2*k-1]
 		playcount = (line[2*k].split(','))[0]
-		rdf_line = "<http://bbc-programmes.dyndns.org/programmes/"+brand+"#episode> "+"<http://purl.org/ontology/playcount/playcount> <"+base+brand+"_"+str(k)+">. <"+base+brand+"_"+str(k)+"> <http://purl.org/ontology/playcount/count> \""+playcount+"\"; <http://purl.org/ontology/playcount/object> <http://dbtune.org/musicbrainz/resource/artist/"+artist+">.\n"
+		rdf_line = "<http://bbc-programmes.dyndns.org/programmes/"+brand+"#episode> "+"<http://purl.org/ontology/playcount/playcount> <"+base+brand+"_"+str(k)+">. <"+base+brand+"_"+str(k)+"> <http://purl.org/ontology/playcount/count> \""+playcount+"\"^^xsd:int; <http://purl.org/ontology/playcount/object> <http://dbtune.org/musicbrainz/resource/artist/"+artist+">.\n"
 		rdf.write(rdf_line)
 		
 f.close()
