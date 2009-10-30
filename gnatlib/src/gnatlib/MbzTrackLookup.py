a8b9bb51c0ca2618780aae74dd7839e89280f704
Yves Raimond <yves@dbtune.org>
2009-10-30 01:30:38 +0000

    Fixing wrong dbtune release URI

--- gnatlib/src/gnatlib/MbzTrackLookup.py
+++ gnatlib/src/gnatlib/MbzTrackLookup.py
@@ -570,7 +570,7 @@ def __setdict__(mbz):
     else:
         dict['artist'] = None
     if mbz.getMbzReleaseURI() !=None:
-        dict['releaseURI'] = DBTUNE_URI+mbz.getMbzReleaseURI().split('http://musicbrainz.org')[1]
+        dict['releaseURI'] = DBTUNE_URI+'/record/' +mbz.getMbzReleaseURI().split('http://musicbrainz.org/release/')[1]
         dict['releaceMbz'] = mbz.getMbzReleaseURI()
     if mbz.track_mapping[0][3] != None:
         dict['release'] = mbz.track_mapping[0][3].release
