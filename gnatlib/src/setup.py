25486f0584356291f9bd8d5956c5279953d6f47e
Kurt Jacobson <kurtjx@gmail.com>
2009-12-07 11:53:28 +0000

    cosmetic changes to setup.py

--- gnatlib/src/setup.py
+++ gnatlib/src/setup.py
@@ -15,12 +15,13 @@ try:
     import musicbrainz2
 except:
     print 'ERROR: musicbrainz2 is not installed - http://musicbrainz.org/doc/python-musicbrainz2_Download'
+    sys.exit(1)
 
 setup(name='gnatlib',
-      version='0.01',
+      version='0.02',
       description='A module for looking up musicbrainz URIs',
       author='Kurt Jacobson',
-      author_email='kurt.jacobson (at) elec.qmul.ac.uk',
+      author_email='kurtjx at gmail.com',
       url='http://omras2.org/gnatlib',
       packages=['gnatlib'],
      )
