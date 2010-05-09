0ead638c07b9616d87c92a4c314aa2942acf4805
Kurt Jacobson <kurtjx@gmail.com>
2010-05-09 16:12:58 +0000

    hoping this works, trying to kill unicode bugs, changes server.py to lastfm_artist_server.py

--- lastfm-artist/src/pylast.py
+++ lastfm-artist/src/pylast.py
@@ -164,7 +164,7 @@ class _Request(object):
 		
 		for name in keys:
 			string += name
-			string += urllib.quote(self.params[name])
+			string += urllib.quote(self.params[name].encode('utf-8'))
 		
 		string += self.api_secret
 		
