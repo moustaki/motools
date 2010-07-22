349b1c60b3afe4d387992a1f56b65aa852fe4fbb
Kurt Jacobson <kurtjx@gmail.com>
2010-07-22 15:31:40 +0000

    fix settings

--- echonest-artists/src/settings.py
+++ echonest-artists/src/settings.py
@@ -4,11 +4,13 @@ Created on Jul 21, 2010
 @author: kurtjx
 '''
 # URIs and such
-URI_BASE = '/'#"http://dbtune.org/artists/echonest/"
+#URI_BASE = '/'
+URI_BASE = "http://dbtune.org/artists/echonest/"
 URI_BASE_MBID = "http://dbtune.org/artists/echonest/mbid/"
 ENID_PREFIX = "music://id.echonest.com/~/AR/"
 METHOD_URI = URI_BASE+'method'
 
 # server settings
-HOST = '127.0.0.1'#'192.168.122.144'
+#HOST = '127.0.0.1'
+HOST = '192.168.122.144'
 PORT = 6767
